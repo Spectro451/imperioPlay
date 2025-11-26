514cb214f21419a317a53f82742b38401aa175f2
Spectro451 <bastyfarias30@gmail.com>
2025-11-25 22:46:38 -0300

    tratando de entender tailwind se armo la base para el navBar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
+import Image from "next/image";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "ImperioPlay",
@@ -13,14 +15,50 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>
-        <header>ImperioPlay</header>
-        <nav>
-          <p>Producto</p>
-          <p>Producto</p>
-          <p>Producto</p>
-          <p>Producto</p>
-          <p>Producto</p>
+        <header className="relative w-full h-48">
+          <Image
+            src="/banner.jpg"
+            alt="Banner"
+            fill
+            style={{ objectFit: "cover", objectPosition: "top center" }}
+          />
+        </header>
+        <nav className="flex justify-around items-center bg-blue-500">
+          <Link href="/" className="block p-2 text-white text-center w-full">
+            Inicio
+          </Link>
+
+          <div className="relative w-full">
+            <Link
+              href="/productos"
+              className="block p-2 text-white text-center "
+            >
+              Productos
+            </Link>
+          </div>
+
+          <Link
+            href="/juegos"
+            className="block p-2 text-white text-center w-full"
+          >
+            Juegos
+          </Link>
+
+          <Link
+            href="/consolas"
+            className="block p-2 text-white text-center w-full"
+          >
+            Consolas
+          </Link>
+
+          <Link
+            href="/ofertas"
+            className="block p-2 text-white text-center w-full"
+          >
+            Ofertas
+          </Link>
         </nav>
+
         {children}
         <footer>@Derechos recerbados</footer>
       </body>
