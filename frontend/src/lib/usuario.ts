b3d7738323567194828176089216e9ccab1d408a
Spectro451 <bastyfarias30@gmail.com>
2025-11-27 23:46:18 -0300

    fetch usuario completo

--- frontend/src/lib/usuario.ts
+++ frontend/src/lib/usuario.ts
@@ -1,13 +1,15 @@
 import { Rol } from "@/types/enums";
+import { Usuario } from "@/types/usuario";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+//crear usuario
 export async function postUsuario(data: {
   nombre: string;
   correo: string;
   password: string;
   rol?: Rol;
-}) {
+}): Promise<Usuario> {
   const res = await fetch(`${API_URL}/usuario`, {
     method: "POST",
     headers: {
@@ -19,7 +21,8 @@ export async function postUsuario(data: {
   return res.json();
 }
 
-export async function getall() {
+//getUsuarios
+export async function getUsuarios(): Promise<Usuario[]> {
   const token = localStorage.getItem("token");
   if (!token) throw new Error("Usuario no autorizado");
 
@@ -29,5 +32,86 @@ export async function getall() {
     },
   });
   if (!res.ok) throw new Error("Error al cargar usuarios");
+  return res.json();
+}
+
+//getId
+export async function getUsuarioId(id: number): Promise<Usuario> {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("Usuario no autorizado");
+
+  const res = await fetch(`${API_URL}/usuario/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) throw new Error("Error al cargar usuario");
+  return res.json();
+}
+
+export async function editarUsuario(
+  id: number,
+  data: Partial<Usuario>
+): Promise<Usuario> {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("Usuario no autorizado");
+
+  const res = await fetch(`${API_URL}/usuario/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) throw new Error("Error al editar usuario");
+  return res.json();
+}
+
+export async function deleteUsuario(id: number): Promise<Usuario> {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("Usuario no autorizado");
+
+  const res = await fetch(`${API_URL}/usuario/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) throw new Error("Error al cargar usuario");
+  return res.json();
+}
+
+export async function cambiarContraseña(
+  id: number,
+  data: { currentPassword: string; password: string }
+): Promise<Usuario> {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("Usuario no autorizado");
+
+  const res = await fetch(`${API_URL}/usuario/${id}/password`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) throw new Error("Error al cambiar contraseña");
+  return res.json();
+}
+
+export async function recuperarCuenta(data: {
+  correo: string;
+  password: string;
+}): Promise<Usuario> {
+  const res = await fetch(`${API_URL}/usuario/restore`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) throw new Error("Error al reestablecer usuario");
   return res.json();
 }
